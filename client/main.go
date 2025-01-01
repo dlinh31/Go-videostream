1111e2ee9e7fbd37bf3f7ed934228ec6c848c381
Liam Ha <haduylinh2005.work@gmail.com>
2025-01-01 18:47:01 -0500

    send video stream

--- client/main.go
+++ client/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"io"
 	"log"
 	"time"
 
@@ -29,5 +30,29 @@ func main(){
 		log.Fatalf("Error when calling ListVideos: %v", err)
 	}
 	log.Printf("Available videos: %v", res.Videos)
+
+	videoName := "sample2.mp4"
+	streamVideo(client, videoName)
+
+}
+
+func streamVideo(client pb.VideoStreamServiceClient, videoName string){
+	req := &pb.VideoRequest{VideoName: videoName}
+	stream, err := client.StreamVideo(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Failed to stream video: %v", err)
+	}
+	log.Printf("Start streaming video: %s", videoName)
+	for {
+		chunk, err := stream.Recv()
+		if err == io.EOF {
+			log.Println("Video streaming completed")
+			break
+		}
+		if err != nil {
+			log.Fatalf("Error while receiving video chunk: %v", err)
+		}
+		log.Printf("Received chunk of size: %d bytes", len(chunk.ChunkData))
+	}
 
 }
--- server/main.go
+++ server/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"os"
+	"path/filepath"
+	"time"
 
 	pb "github.com/dlinh31/go-videostream/proto"
 	"google.golang.org/grpc"
@@ -34,9 +37,34 @@ func (s *VideoStreamServer) ListVideos(ctx context.Context, req *pb.NoParam) (*p
 	return &pb.VideoList{Videos: videos}, nil
 }
 
-// func (s *VideoStreamServer) StreamVideo (ctx context.Context, req *pb.VideoRequest, stream pb.VideoStreamService_StreamVideoServer) error {
-// 	return nil
-// }
+func (s *VideoStreamServer) StreamVideo(req *pb.VideoRequest, stream pb.VideoStreamService_StreamVideoServer) error {
+	videoPath := filepath.Join(s.videoDir, req.VideoName)
+	file, err := os.Open(videoPath)
+	if err != nil {
+		log.Fatalf("Cannot open file with given directory in server %v", err)
+	}
+	defer file.Close()
+	chunkSize := 1024 * 2024 // 1MB
+	buffer := make([]byte, chunkSize)
+	for {
+		n, err := file.Read(buffer)
+		if err == io.EOF{
+			break
+		}
+		if err != nil {
+			log.Fatalf("Error while read video in server %v", err)
+			return err
+		}
+		if err := stream.Send(&pb.VideoChunk{ChunkData: buffer[:n]}); err != nil {
+			log.Fatalf("Error while sending video chunk in server %v", err)
+			return err
+		}
+		time.Sleep(500 * time.Millisecond) // 500ms delay
+
+	}
+	return nil
+
+}
 
 
 func main(){
